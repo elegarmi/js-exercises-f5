b98884e7571c54130cbb902ad63540009703f175
elegarmi <elegarmi@gmail.com>
2022-01-13 15:59:28 +0100

    Updated exercise 10 and added exercise 11

--- 10-count-vowels/app.js
+++ 10-count-vowels/app.js
@@ -2,23 +2,5 @@ let container = document.getElementById("container");
 let phrase = prompt("Enter a phrase");
 let regex = /[aeiou]/ig;
 let vowels = phrase.match(regex);
-let allA = [];
-let allE = [];
-let allI = [];
-let allO = [];
-let allU = [];
 
-vowels.forEach(vowel => {
-    if(vowel.toLowerCase() === 'a') { allA.push(vowel); }
-    if(vowel.toLowerCase() === 'e') { allE.push(vowel); }
-    if(vowel.toLowerCase() === 'i') { allI.push(vowel); }
-    if(vowel.toLowerCase() === 'o') { allO.push(vowel); }
-    if(vowel.toLowerCase() === 'u') { allU.push(vowel); }
-});
-
-container.innerHTML = `There are ${vowels.length} vowels in this phrase, of which: <br>`;
-container.innerHTML += `${allA.length} are a <br>`;
-container.innerHTML += `${allE.length} are e <br>`;
-container.innerHTML += `${allI.length} are i <br>`;
-container.innerHTML += `${allO.length} are o <br>`;
-container.innerHTML += `${allU.length} are u <br>`;
+container.innerHTML = `There are ${vowels.length} vowels in this phrase`;
