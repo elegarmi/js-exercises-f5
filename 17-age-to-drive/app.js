e06c2347c4ec6ed15f08730cc812d30b3c06e55a
elegarmi <elegarmi@gmail.com>
2022-01-16 17:41:50 +0100

    Refactorized exercise 17

--- 17-age-to-drive/app.js
+++ 17-age-to-drive/app.js
@@ -1,10 +1,4 @@
 let container = document.getElementById("container");
 let age = parseInt(prompt('Enter your age'));
 
-if(age > 18) {
-    container.innerHTML = 'You can drive';
-}
-
-else {
-    container.innerHTML = 'You cannot drive';
-}
+container.innerHTML = driver = (age < 18) ? "You cannot drive" : "You can drive";
