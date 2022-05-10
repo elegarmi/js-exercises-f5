77bd957473de5bd321b72f54a33dc7e133be6fee
elegarmi <elegarmi@gmail.com>
2022-05-10 13:20:49 +0200

    11 refactorized

--- 11-count-each-vowel/app.js
+++ 11-count-each-vowel/app.js
@@ -1,24 +1,20 @@
-let container = document.getElementById("container");
-let phrase = prompt("Enter a phrase");
-let regex = /[aeiou]/ig;
-let vowels = phrase.match(regex);
-let allA = [];
-let allE = [];
-let allI = [];
-let allO = [];
-let allU = [];
+let container = document.getElementById("container")
+let phrase = prompt("Enter a phrase")
+let regex = /[aeiou]/ig
+let lowerPhrase = phrase.toLowerCase()
+let vowels = lowerPhrase.match(regex)
+let countVowels = vowels.reduce((countVowels, vowels) => {
+    countVowels[vowels] ? countVowels[vowels] = countVowels[vowels] + 1 : countVowels[vowels] = 1
 
-vowels.forEach(vowel => {
-    if(vowel.toLowerCase() === 'a') { allA.push(vowel); }
-    if(vowel.toLowerCase() === 'e') { allE.push(vowel); }
-    if(vowel.toLowerCase() === 'i') { allI.push(vowel); }
-    if(vowel.toLowerCase() === 'o') { allO.push(vowel); }
-    if(vowel.toLowerCase() === 'u') { allU.push(vowel); }
-});
+    return countVowels
 
-container.innerHTML = `There are ${vowels.length} vowels in this phrase, of which: <br>`;
-container.innerHTML += `${allA.length} are a <br>`;
-container.innerHTML += `${allE.length} are e <br>`;
-container.innerHTML += `${allI.length} are i <br>`;
-container.innerHTML += `${allO.length} are o <br>`;
-container.innerHTML += `${allU.length} are u <br>`;
+}, {})
+
+container.innerHTML = `There are ${vowels.length} vowels in this phrase: <br>`
+
+Object.entries(countVowels).forEach(entry => {
+    const key = entry[0]
+    const value = entry[1]
+
+    container.innerHTML += `${key}: ${value} <br>`
+})
